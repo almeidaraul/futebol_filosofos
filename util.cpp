bba96e84ce8d6b9433f0fe95e89228dbae35a10c
Arthur Martinelli <ama18@inf.ufpr.br>
2021-08-09 18:54:12 -0300

    rascunho da ia feita

--- util.cpp
+++ util.cpp
@@ -26,8 +26,9 @@ void config::insert_player(int i) {
 		field[i] = PLAYER;
 }
 
-vector<tile>::iterator config::moveBola(vector <tile> tabuleiro, int direction, vector<tile>::iterator ball_pos) {
+vector<tile>::iterator config::moveBolaIterador(vector <tile> tabuleiro, int direction) {
 
+		auto ball_pos = find(tabuleiro.begin(), tabuleiro.end(), BALL);
 		while (*(ball_pos + direction) == PLAYER) {
 				auto destination = ball_pos + direction;
 				while (*destination == PLAYER)
@@ -40,14 +41,32 @@ vector<tile>::iterator config::moveBola(vector <tile> tabuleiro, int direction, 
 		return ball_pos;
 
 }
+vector<int> config::moveBolaVetor(vector <tile> tabuleiro, int direction) {
+
+        vector<int> v;
+
+		auto ball_pos = find(tabuleiro.begin(), tabuleiro.end(), BALL);
+
+		while (*(ball_pos + direction) == PLAYER) {
+				auto destination = ball_pos + direction;
+				while (*destination == PLAYER)
+						destination += direction;
+                v.push_back(distance(tabuleiro.begin(), destination));
+				fill(ball_pos, destination, EMPTY);
+				*destination = BALL;
+				ball_pos = destination;
+		}
+
+		return v;
+
+}
 
 bool config::fazGol(int direction) {
 
 		vector <tile> field_copia = this->field;
-		auto ball_pos = find(field_copia.begin(), field_copia.end(), BALL);
 		auto gol_pos = (this->s == LEFT) ? field_copia.end()-1 : field_copia.begin();
 
-		if (distance(moveBola(field_copia, direction, ball_pos), gol_pos) == 0)
+		if (distance(moveBolaIterador(field_copia, direction), gol_pos) == 0)
 				return true;
 		return false;
 
@@ -58,7 +77,7 @@ bool config::haDoisOuMaisSaltos(int direction) {
 		vector <tile> field_copia = this->field;
 		auto ball_pos = find(field_copia.begin(), field_copia.end(), BALL);
 
-		if (abs(distance(moveBola(field_copia, direction, ball_pos), ball_pos)) > 2)
+		if (abs(distance(moveBolaIterador(field_copia, direction), ball_pos)) > 2)
 				return true;
 		return false;
 
@@ -67,32 +86,49 @@ bool config::haDoisOuMaisSaltos(int direction) {
 bool config::caiPosicaoMorta(int direction) {
 
 		vector <tile> field_copia = this->field;
-		auto ball_pos = find(field_copia.begin(), field_copia.end(), BALL);
 		auto gol_pos = (this->s == LEFT) ? field_copia.end()-1 : field_copia.begin();
 
-		if (distance(moveBola(field_copia, direction, ball_pos), gol_pos) == 1)
+		if (distance(moveBolaIterador(field_copia, direction), gol_pos) == 1)
 				return true;
 		return false;
 
 }
 
+int melhorPosicaoFilosofo(vector <tile> tabuleiro, int direction) {
+
+        
+		auto ball_pos = find(this->tabuleiro.begin(), this->tabuleiro.end(), BALL);
+        while (*(ball_pos + direction) == PLAYER) {
+            while (*(ball_pos + direction) == PLAYER)
+                ball_pos = ball_pos + direction;
+            ball_pos = ball_pos+direction;
+        }
+
+        return (distance(tabuleiro.begin(), ball_pos) + direction)
+}
+
 void config::play() {
 		
-		auto ball_pos = find(this->field.begin(), this->field.end(), BALL);
+		vector <tile> field_copia = this->field;
 		int direction = (this->s == LEFT) ? 1 : -1;
+		auto gol_pos = (this->s == LEFT) ? field_copia.end()-1 : field_copia.begin();
 
-		/*
+		
 		if (fazGol(direction))
-				//move a bola pro gol
+            write_jumps(moveBolaVetor(field_copia, direction));
 		else if (fazGol(-direction))    //se o oponente pode fazer gol
+            write_jumps(moveBolaVetor(field_copia, direction));
 				//empurra a bola pra frente
 		else if (caiPosicaoMorta())     //se cai exatamente ao lado do gol
+            write_player(distance(field_copia.begin(), gol_pos) - direction);
 				//coloca um filosófo na posição antes do gol
 		else if (haDoisOuMaisSaltos(direction))
+            write_jumps(moveBolaVetor(field_copia, direction));
 				//manda a bola o mais pra frente possível
 		else
+            write_player(melhorPosicaoFilosofo(field_copia, direction));
 				//coloca filosofo alternado
-				*/
+				
 }
 
 config read_config() {
